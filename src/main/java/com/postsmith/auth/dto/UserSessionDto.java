fa057c723b1c7731f53efd9f6711216cd5adc163
chauid <dlfdl28@naver.com>
2025-06-18 05:43:27 +0900

    세션 인증 로직 추가

--- src/main/java/com/postsmith/auth/dto/UserSessionDto.java
+++ src/main/java/com/postsmith/auth/dto/UserSessionDto.java
@@ -1,12 +1,20 @@
 package com.postsmith.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserSessionDto {
+	private String accessToken;
 	private String userId;
-	private String sessionId;
-	private String userName;
 	private String email;
 	private String role;
+	private String userNickname;
+	private String profileImage;
 }
